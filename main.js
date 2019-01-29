f9ca550f4ffcc43a33f75a37b3dd1f6fd6ac1c90
Chonchanok MueangRuangWit <iamcmnut@gmail.com>
2019-01-29 20:54:15 +0700

    week 3: character control

--- main.js
+++ main.js
@@ -2,80 +2,132 @@
 var canvas = document.getElementById("myCanvas");
 
 // set canvas size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 500;
+canvas.height = 500;
 
 // get context
 var ctx = canvas.getContext("2d");
 
-// Circle class
-class Circle {
-  constructor(posX, posY, radius, speedX, speedY) {
+// Snake class
+class Snake {
+  /**
+   * Snake class contructor
+   * @param {number} posX initial X position
+   * @param {number} posY initial Y position
+   * @param {number} scale snake scale size
+   * @param {number} initTailSize initial snake tail size
+   */
+  constructor(posX, posY, scale, initTailSize = 4) {
     this.posX = posX;
     this.posY = posY;
-    this.radius = radius;
-    this.speedX = speedX;
-    this.speedY = speedY;
+    this.scale = scale;
+    this.tail = [];
+    // generate tail from initTailSize
+    for (var i = 0; i < initTailSize; i++) {
+      this.tail.push({x: this.posX, y: this.posY});
+    }
   }
 
   // Draw circle
   draw() {
-    ctx.beginPath();
-    ctx.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2);
-    ctx.strokeStyle = "black";
-    ctx.stroke();
-    ctx.fillStyle = "black";
+    // display snake head
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.posX, this.posY, this.scale, this.scale);
     ctx.fill();
+
+    // display snake tail
+    for (var i = 0; i < this.tail.length; i++) {
+      ctx.fillStyle = "green";
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, this.scale, this.scale);
+      ctx.fill();
+    }
   }
 
-  // Update
-  update() {
-    if (this.posX + this.radius > window.innerWidth || this.posX - this.radius < 0) {
-      this.speedX = -this.speedX;
+  // move left
+  moveLeft() {
+    this.posX = this.posX - this.scale;
+    if (this.posX < 0) {
+      this.posX = canvas.width - this.scale;
     }
+    this.updateTail();
+  }
 
-    if (this.posY + this.radius > window.innerHeight || this.posY - this.radius < 0) {
-      this.speedY = -this.speedY;
+  moveRight() {
+    this.posX = this.posX + this.scale;
+    if (this.posX >= canvas.width) {
+      this.posX = 0;
     }
+    this.updateTail();
+  }
 
-    this.posX = this.posX + this.speedX;
-    this.posY = this.posY + this.speedY;
+  moveUp() {
+    this.posY = this.posY - this.scale;
+    if (this.posY < 0) {
+      this.posY = canvas.height - this.scale;
+    }
+    this.updateTail();
+  }
+
+  moveDown() {
+    this.posY = this.posY + this.scale;
+    if (this.posY >= canvas.height) {
+      this.posY = 0;
+    }
+    this.updateTail();
+  }
+
+  // update snake tail position
+  updateTail() {
+    // move the last tail position to the first position
+    // and shift the others to the right
+    this.tail.unshift(this.tail.pop());
+
+    // set the 1st position with snake's head position
+    this.tail[0] = {x: this.posX, y: this.posY};
   }
 }
 
 // Circle array
-var circleArr = [];
-
-// generate random circles
-for (var i = 0; i < 100; i++) {
-  var posX = Math.random() * window.innerWidth;
-  var posY = Math.random() * window.innerHeight;
-  var radius = Math.random() * 50;
-  var speedX = Math.random() * 5;
-  var speedY = Math.random() * 5;
-
-  // create circle object from Circle class
-  var circle = new Circle(posX, posY, radius, speedX, speedY);
-
-  // push new circle object to circle array
-  circleArr.push(circle);
-}
+var snake = new Snake(100, 100, 25);
 
 // Animation controller
 function animation() {
 
   // Clear canvas
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // update circle
-  for (var i = 0; i < circleArr.length; i++) {
-    circleArr[i].draw();
-    circleArr[i].update();
-  }
+  snake.draw();
 
   // refresh screen with window.requestAnimationFrame()
   window.requestAnimationFrame(animation);
 }
 
+// keyboard down handler
+function keyDownHandler(key) {
+  // arrow key LEFT
+  if (key.keyCode == 37) {
+    snake.moveLeft();
+  }
+
+  // arrow key UP
+  if (key.keyCode == 38) {
+    snake.moveUp();
+  }
+
+  // arrow key RIGHT
+  if (key.keyCode == 39) {
+    snake.moveRight();
+  }
+
+  // arrow key DOWN
+  if (key.keyCode == 40) {
+    snake.moveDown();
+  }
+}
+
 // call animation()
 animation();
+
+// bind keyDownHandler with browser's eventListener
+window.addEventListener('keydown', keyDownHandler);
